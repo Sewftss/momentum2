949f942d6f5d9b2629df3494a3a713f4c8f38da3
Sewftss <Rodolfo@MacBook-Air-de-Rodolfo.local>
2017-09-06 10:06:54 +0200

    mise à jour

---
+++ imports/ui/body.js
@@ -0,0 +1,242 @@
+
+
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+ 
+import './body.html';
+ 
+
+
+for(let k=0;k<=1;k++){
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"janv.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+ "fev";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+ "mars";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 30;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"avr.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"mai";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 30;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"juin";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+
+}
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"juil.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"août";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+
+for(let i=1;i<= 30;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"sept.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"oct.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+
+
+}
+
+for(let i=1;i<= 30;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"nov.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+for(let i=1;i<= 31;i++){
+  let nb = document.createElement("span");
+  if (i==1){
+    nb.innerHTML=i+"dec.";
+  }
+  else {
+    nb.innerHTML=i;
+  }
+  nb.style.fontSize=30+"px";
+  nb.style.color="#999999"
+  nb.style.fontFamily="arial";
+  nb.style.padding=15+"px";
+  nb.style.border="solid black 1px";
+  nb.style.background="white";
+  document.body.appendChild(nb);
+
+}
+
+
+}
+//-----------------------------------------
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+
